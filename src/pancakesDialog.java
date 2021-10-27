c7d725d26518dccce3ae577556ab80d67502388b
Mila Yonkova <48622362+MilaDimitrova@users.noreply.github.com>
2021-10-27 18:23:42 +0300

    Започната задача Pancakes

---
+++ src/pancakesDialog.java
@@ -0,0 +1,33 @@
+public class pancakesDialog {
+    public static void main(String[] args) {
+
+        System.out.println("Изберете палачинка: ");
+        System.out.println("1. Солена палачинка");
+        System.out.println("2. Сладка палачинка");
+        System.out.println();
+
+        //тук потребителя избира 1 или 2
+        System.out.println("Моля изберете добавки : ");
+        System.out.println();
+        // шунка | цена 0,50ст.
+        // кашкавал | цена 0,50ст.
+        // сирене | цена 0,50ст.
+        // шоколад | цена 0,50ст.
+        // сладко | цена 0,50ст.
+        // банан | цена 0,50ст.
+
+        //тук клиента избира ....,....,..., | не искам добавки
+        System.out.println("*------------------------------*");
+        System.out.println("  *--------------------------*");
+        System.out.println("          ПРИГОТВЯНЕ  ");
+        System.out.println("  *--------------------------*");
+        System.out.println("*------------------------------*");
+        System.out.println("  *--------------------------*");
+        System.out.println("         МОЛЯ ИЗЧАКАЙТЕ  ");
+        System.out.println("  *--------------------------*");
+        System.out.println("*------------------------------*");
+        System.out.println();
+        System.out.println();
+        System.out.println("Дължите ---лв.");
+    }
+}
