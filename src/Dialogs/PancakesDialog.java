0bbae3da6e204af11e0a3bb25aa014514952b260
Mila Yonkova <48622362+MilaDimitrova@users.noreply.github.com>
2021-11-17 20:23:25 +0200

    Fix the problem with skiping topping

--- src/Dialogs/PancakesDialog.java
+++ src/Dialogs/PancakesDialog.java
@@ -1,17 +1,20 @@
 package Dialogs;
 
 import menuItems.Pancakes;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class PancakesDialog{
+public class PancakesDialog {
     public Scanner scan = new Scanner(System.in);
-    public void  choosePancake() {
+
+    public void choosePancake() {
         while (true) {
             System.out.println("Това са добавките ,от които можете да изберете!");
             System.out.println();
             Pancakes pancake = new Pancakes();
             pancake.printTopings();
-            System.out.println("Моля изберете добавки за вашата палачинка.");
+            System.out.println("Искате ли добавки за вашата палачинка?");
             String[] toppings = new String[4];
             System.out.println("Моля изберете кашкавал: ");
             toppings[0] = scan.nextLine();
@@ -25,23 +28,23 @@ public class PancakesDialog{
             System.out.println("Това ли ще е вашата поръчка?");
             String ans = scan.next();
             ans.toLowerCase();
-            if (ans.equalsIgnoreCase("да")){
+            if (ans.equalsIgnoreCase("да")) {
                 break;
-            }else{
+            } else {
                 System.out.println("Искате ли да поръчате още една палачинка?");
                 ans = scan.next();
                 ans.toLowerCase();
-                if (ans.contains("не")){
+                if (ans.contains("не")) {
                     System.out.println("Искате ли да поръчате нещо друго oт нашето меню?");
                     ans = scan.next();
                     ans.toLowerCase();
-                    if(ans.contains("да")){
+                    if (ans.contains("да")) {
                         ClientDialog dialog = new ClientDialog();
                         dialog.chooseProduct();
                     }
                     break;
                 }
-                if (ans.contains("да")){
+                if (ans.contains("да")) {
                     choosePancake();
                 }
             }
